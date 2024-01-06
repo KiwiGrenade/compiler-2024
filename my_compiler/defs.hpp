ebc91cdd1af6a2f252c9af45430bf7f128047a81
Mango T14 <kubajaskow@wp.pl>
2024-01-07 00:53:10 +0100

    Add safer pointer handling

--- my_compiler/defs.hpp
+++ my_compiler/defs.hpp
@@ -2,13 +2,19 @@
 #define DEFS_HPP
     #include <string>
     #include <iostream>
+    #include <memory>
+
+    #define ptr(type) std::shared_ptr<type>
+    #define new_ptr(type, ...) std::make_shared<type>(__VA_ARGS__)
 
     extern int yylex();
     extern "C" int yywrap();
-
     void yyerror(const char* string);
     int yylex();
     int yyparse();
+    
+    typedef std::string ident;
+
     extern size_t currLine;
 
 #endif
