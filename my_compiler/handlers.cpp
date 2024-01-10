adde4c4335c1f6fd8289c6850e5bbd9c68cb905d
Mango T14 <kubajaskow@wp.pl>
2024-01-10 17:49:05 +0100

    Add handleProcCall()

--- my_compiler/handlers.cpp
+++ my_compiler/handlers.cpp
@@ -5,6 +5,7 @@
 size_t curr_vertex_id = 0;
 std::vector<CodeBlock> AST::vertices;
 std::vector<EdgeProvider> providers;
+std::vector<ident> procedures;
 
 // void chuj() {
 //     std::cout << "chuj" << std::endl;
@@ -34,8 +35,8 @@ ident handleCondition(ident VAL1, int INS_TYPE, ident VAL2) {
     provider.set_begin_id(id);
     provider.set_end_id(id);
     providers.push_back(provider);
-    curr_vertex_id++;
     
+    curr_vertex_id++;
     return std::to_string(curr_vertex_id - 1);
 }
 
@@ -53,8 +54,8 @@ ident handleRepeat(ident COMMANDS_ID, ident CONDITION_ID) {
     EdgeProvider provider;
     provider._begin_id = providers[cond_id]._begin_id;
     providers.push_back(provider);
+    
     curr_vertex_id++;
-
     return std::to_string(curr_vertex_id - 1);
 }
 
@@ -74,5 +75,77 @@ ident handleIf(ident CONDITION_ID, ident COMMANDS_ID) {
     provider._end_id = curr_vertex_id;
     providers.push_back(provider);
 
+    curr_vertex_id++;
     return std::to_string(curr_vertex_id - 1);
+}
+
+ident handleIfElse(ident CONDITION_ID, ident IF_COMMANDS_ID, ident ELSE_COMMANDS_ID) {
+    int cond_id = stoi(CONDITION_ID);
+    int if_comms_id = stoi(IF_COMMANDS_ID);
+    int el_comms_id = stoi(ELSE_COMMANDS_ID);
+
+    AST::add_vertex(curr_vertex_id);
+    AST::vertices[AST::vertices.size() - 1].empty = 1;
+
+    //TODO: this may be problematic
+    AST::add_edge(providers[cond_id]._begin_id, providers[if_comms_id]._begin_id, true);
+    AST::add_edge(providers[cond_id]._begin_id, providers[el_comms_id]._begin_id, false);
+    AST::add_edge(providers[if_comms_id]._end_id, curr_vertex_id);
+    AST::add_edge(providers[el_comms_id]._end_id, curr_vertex_id);
+
+    EdgeProvider provider;
+    provider._begin_id = providers[cond_id]._begin_id;
+    provider._end_id = curr_vertex_id;
+    providers.push_back(provider);
+
+    curr_vertex_id++;
+    return std::to_string(curr_vertex_id - 1);   
+}
+
+
+ident handleProcCall(ident PROC_CALL) {
+    ident proc_name;
+    ident tmp_arg_name;
+    Instruction instruction;
+    instruction.type_of_instruction = content_type::_CALL;
+    
+    //TODO: check if you can optimise this
+    //TODO: include tables into arguments
+    bool is_function = false;
+    for(auto c : PROC_CALL) {
+        if (c == '(') {
+            is_function = true;
+        }
+        if (is_function) {
+            if (c == ',' || c == ')') {
+                instruction.args.push_back(Value(tmp_arg_name));
+                tmp_arg_name = "";
+            }
+            else {
+                tmp_arg_name += c;
+            }
+        }
+        else {
+            proc_name += c;
+        }
+    }
+
+    proc_name += '_' + std::to_string(instruction.args.size());
+    
+    //TODO: you can optimize this
+    for (auto p : procedures) {
+        if(p == proc_name) { 
+            instruction.proc_id = proc_name;
+            AST::add_vertex(curr_vertex_id);
+            AST::vertices[AST::vertices.size() - 1].instructions.push_back(instruction);
+            
+            EdgeProvider provider;
+            provider.set_begin_id(curr_vertex_id);
+            provider.set_end_id(curr_vertex_id);
+            providers.push_back(provider);
+            
+            curr_vertex_id++;
+            return std::to_string(curr_vertex_id - 1);
+        }
+    }   
 }
