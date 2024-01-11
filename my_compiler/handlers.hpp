8b392fba17516a32ce64719b5d5b03bc403510ad
Mango T14 <kubajaskow@wp.pl>
2024-01-11 16:02:09 +0100

    Add declarations for handleMain1/2, handleProcedures1/2 and handleCommands

--- my_compiler/handlers.hpp
+++ my_compiler/handlers.hpp
@@ -12,6 +12,12 @@ ident handleWhile(ident CONDITION_ID, ident COMMAND_ID);
 ident handleProcCall(ident PROC_CALL_ID);
 ident handleAssignment(ident IDENTIFIER_ID, ident EXPRESSION_ID);
 ident handleWhile(ident CONDITION_ID, ident COMMANDS_ID);
+ident handleCommands(ident COMMANDS_ID, ident COMMAND_ID);
+
+void handleMain1();
+void handleMain2(ident DECLARATIONS_ID, ident COMMANDS_ID);
+void handleProcedures1(ident PROCEDURES_ID, ident PROC_HEAD, ident COMMANDS_ID);
+void handleProcedures2(ident PROCEDURES_ID, ident PROC_HEAD, ident DECLARATIONS_ID, ident COMMANDS_ID);
 
 #define handleExpression handleCondition
 #define handleWrite handleCondition
